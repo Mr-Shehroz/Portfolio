3a08ef338928ec5feebf6b551f9c0654a5466325
Mr-Shehroz <shehrozkhan.wow@gmail.com>
2025-07-10 12:48:27 +0500

    add

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
 
 
                     {/* Mobile Menu Button */}
-                    <div className="lg:hidden z-50">
+                    <div className="md:hidden z-50">
                         <button
                             onClick={() => setMenuOpen(!menuOpen)}
                             className="text-white focus:outline-none"
@@ -39,8 +39,8 @@ export default function Header() {
                     </div>
 
                     {/* Nav Menu */}
-                    <nav className={`lg:flex ${menuOpen ? 'block absolute w-full left-0 right-0 top-16 bg-[#5B1F69] bg-opacity-80 rounded-lg p-4' : 'hidden'} 2xl:text-xl xl:text-[16px] lg:text-[14px] text-[10px]`}>
-                        <ul className="flex flex-col lg:flex-row lg:space-x-6 space-y-2 lg:space-y-0">
+                    <nav className={`md:flex ${menuOpen ? 'block absolute w-full left-0 right-0 top-16 bg-[#5B1F69] bg-opacity-80 rounded-lg p-4' : 'hidden'} 2xl:text-xl xl:text-[16px] lg:text-[14px] text-[10px]`}>
+                        <ul className="flex flex-col md:flex-row md:space-x-6 space-y-2 md:space-y-0">
                             <li>
                                 <a href="#home" className="text-white hover:text-yellow-400 transition duration-300">Home</a>
                             </li>
@@ -56,7 +56,7 @@ export default function Header() {
                         </ul>
                     </nav>
                 </div>
-                <section className='2xl:max-w-7xl xl:max-w-6xl lg:max-w-5xl max-w-2xl mx-auto flex flex-col md:flex-row items-center justify-between 2xl:pt-50 lg:pt-30 pt-20 h-[85vh] px-6'>
+                <section className='2xl:max-w-7xl xl:max-w-6xl lg:max-w-5xl max-w-2xl mx-auto flex flex-col md:flex-row items-center md:justify-between gap-20 md:gap-0 2xl:pt-50 lg:pt-30 pt-20 h-[85vh] px-6'>
                     <div className='flex flex-col items-start justify-center max-w-[300px] lg:max-w-[510px] xl:max-w-[600px] 2xl:max-w-[700px]'>
                         <h2 className={`2xl:text-[90px] xl:text-[50px] lg:text-[44px] text-[25px] font-bold lg:leading-18 xl:leading-25 ${josefinSans.className}`}>Hi, I`m Shehroz</h2>
                         <h3 className={`2xl:text-[50px] lg:text-[30px] text-[16px] font-semibold bg-gradient-to-r from-[#5DFFFF] to-[#AE0CA7] bg-clip-text text-transparent ${josefinSans.className}`}>Full-Stack Developer.</h3>
@@ -70,7 +70,7 @@ export default function Header() {
 
                     </div>
                     <div className='w-full'>
-                        <Image className='max-w-[545px] w-full' src="/" height={100} width={100} alt='logo' />
+                        <Image className='2xl:max-w-[545px] xl:max-w-[440px] lg:max-w-[330px] md:max-w-[230px] max-w-full w-full' src="/boy.webp" height={100} width={100} alt='logo' />
                     </div>
                 </section>
             </header>
